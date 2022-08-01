66e7a47a730a675e8fdb9e9f8c071d827607a66a
Ibraheem Rodrigues <28712361+IbraheemR@users.noreply.github.com>
2022-08-01 10:32:39 +0100

    Remove build tasks, add remap=false

--- src/main/java/com/ibraheemrodrigues/sid/mixin/ChangeServerBrandMixin.java
+++ src/main/java/com/ibraheemrodrigues/sid/mixin/ChangeServerBrandMixin.java
@@ -1,18 +1,18 @@
 package com.ibraheemrodrigues.sid.mixin;
 
 import com.ibraheemrodrigues.sid.SIDMain;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
 public class ChangeServerBrandMixin {
-	@Inject(at = @At("RETURN"), method = "getServerModName", cancellable = true)
+	// MinecraftServer.getServerModName() uses @DontObfuscate so we need to set remap = false
+	@Inject(at = @At("RETURN"), method = "getServerModName", cancellable = true, remap = false)
 	private void getServerModName(CallbackInfoReturnable<String> cir) {
 		if (SIDMain.CUSTOM_BRAND != null) cir.setReturnValue(SIDMain.CUSTOM_BRAND);
+
 	}
 }
